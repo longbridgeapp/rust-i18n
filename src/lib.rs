2d5932a874d41d29d9d85c4137ce05c1b04993d3
Jason Lee <huacnlee@gmail.com>
2021-12-06 11:53:53 +0800

    Refactor for use i18n! for load locales in manualy.

--- src/lib.rs
+++ src/lib.rs
@@ -56,50 +56,231 @@ rust_i18n::locale();
 // => "zh-CN"
 ```
 */
-include!(concat!(env!("OUT_DIR"), "/i18n.rs"));
+// include!(concat!(env!("OUT_DIR"), "/i18n.rs"));
+use glob::glob;
+use proc_macro2::TokenStream;
+use quote::quote;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_t() {
-        assert_eq!(t!("hello"), "Hello, World!");
-        assert_eq!(t!("hello"), "Hello, World!");
+type Locale = String;
+type Value = serde_json::Value;
+type Translations = HashMap<Locale, Value>;
 
-        // Vars
-        assert_eq!(
-            t!("a.very.nested.message"),
-            "Hello, %{name}. Your message is: %{msg}"
-        );
-        assert_eq!(
-            t!("a.very.nested.message", name = "Jason"),
-            "Hello, Jason. Your message is: %{msg}"
-        );
-        assert_eq!(
-            t!("a.very.nested.message", name = "Jason", msg = "Bla bla"),
-            "Hello, Jason. Your message is: Bla bla"
-        );
+#[derive(Debug)]
+struct Option {
+    locales_path: String,
+}
 
-        crate::set_locale("de");
-        assert_eq!(t!("messages.hello", name = "world"), "Hallo, world!");
+impl syn::parse::Parse for Option {
+    fn parse(input: syn::parse::ParseStream) -> syn::parse::Result<Self> {
+        let locales_path = input.parse::<syn::LitStr>()?.value();
 
-        crate::set_locale("en");
-        assert_eq!(t!("messages.hello", name = "world"), "Hello, world!");
+        Ok(Self { locales_path })
+    }
+}
+
+fn is_debug() -> bool {
+    std::env::var("RUST_I18N_DEBUG").is_ok()
+}
+
+#[proc_macro]
+pub fn i18n(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let option = match syn::parse::<Option>(input) {
+        Ok(input) => input,
+        Err(err) => return err.to_compile_error().into(),
+    };
+
+    let translations = load_locales(&option.locales_path);
+
+    let code = generate_code(translations);
+
+    if is_debug() {
+        println!("Code generated:\n{}", code.to_string());
     }
 
-    #[test]
-    fn it_t_with_locale_and_args() {
-        assert_eq!(t!("hello", locale = "de"), "Hallo Welt!");
-        assert_eq!(t!("hello", locale = "en"), "Hello, World!");
+    code.into()
+}
 
-        crate::set_locale("en");
-        assert_eq!(t!("messages.hello", name = "Jason"), "Hello, Jason!");
-        assert_eq!(
-            t!("messages.hello", locale = "en", name = "Jason"),
-            "Hello, Jason!"
-        );
-        assert_eq!(
-            t!("messages.hello", locale = "de", name = "Jason"),
-            "Hallo, Jason!"
-        );
+fn load_locales(dest: &str) -> Translations {
+    let mut translations: Translations = HashMap::new();
+
+    let locale_path = format!("{}/**/*.yml", dest);
+
+    if is_debug() {
+        println!("cargo:i18n-locale-path={}", &locale_path);
     }
+
+    for entry in glob(&locale_path).expect("Failed to read glob pattern") {
+        let entry = entry.unwrap();
+        if is_debug() {
+            println!("cargo:rerun-if-changed={}", entry.display());
+        }
+
+        let file = File::open(entry).expect("Failed to open the YAML file");
+        let mut reader = std::io::BufReader::new(file);
+        let mut content = String::new();
+
+        reader
+            .read_to_string(&mut content)
+            .expect("Read YAML file failed.");
+
+        let trs: Translations =
+            serde_yaml::from_str(&content).expect("Invalid YAML format, parse error");
+
+        translations.extend(trs)
+    }
+
+    translations
+}
+
+fn extract_vars(prefix: &str, trs: &Value) -> HashMap<String, String> {
+    let mut v = HashMap::<String, String>::new();
+    let prefix = prefix.to_string();
+
+    match &trs {
+        serde_json::Value::String(s) => {
+            v.insert(prefix, s.to_string());
+        }
+        serde_json::Value::Object(o) => {
+            for (k, vv) in o {
+                let key = format!("{}.{}", prefix, k);
+                v.extend(extract_vars(key.as_str(), vv));
+            }
+        }
+        serde_json::Value::Null => {
+            v.insert(prefix, "".into());
+        }
+        serde_json::Value::Bool(s) => {
+            v.insert(prefix, format!("{}", s));
+        }
+        serde_json::Value::Number(s) => {
+            v.insert(prefix, format!("{}", s));
+        }
+        serde_json::Value::Array(_) => {
+            v.insert(prefix, "".into());
+        }
+    }
+
+    v
+}
+
+fn generate_code(translations: Translations) -> proc_macro2::TokenStream {
+    let mut locales = Vec::<TokenStream>::new();
+
+    let mut locale_vars = HashMap::<String, String>::new();
+    for (locale, trs) in translations {
+        let new_vars = extract_vars(locale.as_str(), &trs);
+        locale_vars.extend(new_vars);
+    }
+
+    locale_vars.iter().for_each(|(k, v)| {
+        let k = k.to_string();
+        let v = v.to_string();
+
+        locales.push(quote! {
+            #k => #v,
+        });
+    });
+
+    if is_debug() {
+        println!("cargo:i18n-locales={:#?}", locales);
+    }
+
+    let result = quote! {
+        use std::sync::Mutex;
+        use std::collections::HashMap;
+
+        macro_rules! map {
+            {$($key:expr => $value:expr),+} => {{
+                let mut m = HashMap::new();
+                $(
+                    m.insert($key, $value);
+                )+
+                m
+            }};
+        }
+
+        lazy_static::lazy_static! {
+            static ref LOCALES: HashMap<&'static str, &'static str> = map! [
+                #(#locales)*
+                "" => ""
+            ];
+
+            static ref CURRENT_LOCALE: Mutex<String> = Mutex::new(String::from("en"));
+        }
+
+        pub fn set_locale(locale: &str) {
+            let mut current_locale = CURRENT_LOCALE.lock().unwrap();
+            *current_locale = locale.to_string();
+        }
+
+        pub fn locale() -> String {
+            CURRENT_LOCALE.lock().unwrap().to_string()
+        }
+
+        /// Get I18n text
+        ///
+        /// ```ignore
+        /// // Simple get text with current locale
+        /// t!("greeting"); // greeting: "Hello world" => "Hello world"
+        /// // Get a special locale's text
+        /// t!("greeting", locale = "de"); // greeting: "Hallo Welt!" => "Hallo Welt!"
+        ///
+        /// // With variables
+        /// t!("messages.hello", "world"); // messages.hello: "Hello, {}" => "Hello, world"
+        /// t!("messages.foo", "Foo", "Bar"); // messages.foo: "Hello, {} and {}" => "Hello, Foo and Bar"
+        ///
+        /// // With locale and variables
+        /// t!("messages.hello", locale = "de", "Jason"); // messages.hello: "Hallo, {}" => "Hallo, Jason"
+        /// ```
+        #[macro_export]
+        macro_rules! t {
+            // t!("foo")
+            ($key:expr) => {
+                $crate::translate($crate::locale().as_str(), $key)
+            };
+
+            // t!("foo", locale="en")
+            ($key:expr, locale=$locale:tt) => {
+                $crate::translate($locale, $key)
+            };
+
+            // t!("foo", locale="en")
+            ($key:expr, locale=$locale:tt, $($var_name:tt = $var_val:tt),+) => {
+                {
+                    let mut message = $crate::translate($locale, $key);
+                    $(
+                        message = message.replace(concat!("%{", stringify!($var_name), "}"), $var_val);
+                    )+
+                    message
+                }
+            };
+
+            // t!("foo {} {}", "bar", "baz")
+            ($key:expr, $($var_name:tt = $var_val:tt),+) => {
+                {
+                    let mut message = $crate::translate($crate::locale().as_str(), $key);
+                    $(
+                        message = message.replace(concat!("%{", stringify!($var_name), "}"), $var_val);
+                    )+
+                    message
+                }
+            };
+
+        }
+
+        pub fn translate(locale: &str, key: &str) -> String {
+            let key = format!("{}.{}", locale, key);
+            match LOCALES.get(key.as_str()) {
+                Some(value) => value.to_string(),
+                None => {
+                    key.to_string()
+                }
+            }
+        }
+    };
+
+    result
 }
