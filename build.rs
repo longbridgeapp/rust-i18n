6fba16fa08d3ec422dda3ec36d819a57e4035417
Joaquin Mansilla <joaquinmansilla@fiqus.com>
2023-01-13 10:42:49 -0300

    Use `PWD` env as workdir for build.rs, for fix error while trying to cross compile (#18)

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 // https://github.com/longbridgeapp/rust-i18n/blob/v0.1.6/crates/support/src/lib.rs#L9
 fn workdir() -> Option<String> {
-    // Do not panic if OUT_DIR or workdir not correct.
-    // Because in cargo install OUT_DIR is:
-    // /var/folders/qb/.../T/cargo-installcBf9L5/release/build/rust-i18n-xxx/build-script-build
+    if let Ok(pwd) = std::env::var("PWD") {
+        return Some(pwd);
+    }
+
     let dest = std::env::var("OUT_DIR");
     if dest.is_err() {
         return None;
@@ -11,7 +12,7 @@ fn workdir() -> Option<String> {
 
     let seperator = regex::Regex::new(r"(/target/(.+?)/build/)|(\\target\\(.+?)\\build\\)")
         .expect("Invalid regex");
-    let parts = seperator.split(&dest).collect::<Vec<_>>();
+    let parts = seperator.split(dest.as_str()).collect::<Vec<_>>();
 
     if parts.len() >= 2 {
         return Some(parts[0].to_string());
@@ -21,10 +22,8 @@ fn workdir() -> Option<String> {
 }
 
 fn main() {
-    let workdir = workdir();
-    let workdir = workdir.unwrap_or_else(|| {
-        std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR is empty")
-    });
+    let workdir = workdir().expect("Failed to find work directory");
+
     let locale_path = format!("{workdir}/**/locales/**/*.yml");
 
     for entry in glob::glob(&locale_path).expect("Failed to read glob pattern") {
