9310bcec4650f8ec14998730cb93e2b0e8f1a644
Jason Lee <huacnlee@gmail.com>
2023-06-13 10:32:59 +0800

    Fix unused warning.

--- examples/app/main.rs
+++ examples/app/main.rs
@@ -1,29 +1,32 @@
-use rust_i18n::t;
-
 // Init translations for current crate.
 rust_i18n::i18n!("examples/app/locales");
 
 fn main() {}
 
-#[test]
-fn test_example_app() {
-    rust_i18n::set_locale("en");
-    assert_eq!(t!("hello", name = "Longbridge"), "Hello, Longbridge!");
-    assert_eq!(t!("view.buttons.ok"), "Ok");
-    assert_eq!(t!("view.buttons.cancel"), "Cancel");
-    assert_eq!(
-        t!("view.datetime.about_x_hours", count = "10"),
-        "about 10 hours"
-    );
+#[cfg(test)]
+mod tests {
+    use rust_i18n::t;
 
-    assert_eq!(
-        t!("hello", locale = "fr", name = "Longbridge"),
-        "Bonjour, Longbridge!"
-    );
-    rust_i18n::set_locale("fr");
-    assert_eq!(t!("hello", name = "Longbridge"), "Bonjour, Longbridge!");
-    assert_eq!(
-        t!("view.datetime.about_x_hours", count = "10"),
-        "environ 10 heures"
-    );
+    #[test]
+    fn test_example_app() {
+        rust_i18n::set_locale("en");
+        assert_eq!(t!("hello", name = "Longbridge"), "Hello, Longbridge!");
+        assert_eq!(t!("view.buttons.ok"), "Ok");
+        assert_eq!(t!("view.buttons.cancel"), "Cancel");
+        assert_eq!(
+            t!("view.datetime.about_x_hours", count = "10"),
+            "about 10 hours"
+        );
+
+        assert_eq!(
+            t!("hello", locale = "fr", name = "Longbridge"),
+            "Bonjour, Longbridge!"
+        );
+        rust_i18n::set_locale("fr");
+        assert_eq!(t!("hello", name = "Longbridge"), "Bonjour, Longbridge!");
+        assert_eq!(
+            t!("view.datetime.about_x_hours", count = "10"),
+            "environ 10 heures"
+        );
+    }
 }
