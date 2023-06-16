ea381cb4c04b5b4b3ca3a7b960ddfb276dd12abe
Jason Lee <huacnlee@gmail.com>
2023-06-16 14:56:21 +0800

    Add benchmark for `t!` method.

---
+++ benches/bench.rs
@@ -0,0 +1,18 @@
+use rust_i18n::t;
+
+rust_i18n::i18n!("./tests/locales");
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn bench_t(c: &mut Criterion) {
+    c.bench_function("t", |b| b.iter(|| t!("hello")));
+}
+
+fn bench_t_with_args(c: &mut Criterion) {
+    c.bench_function("t_with_args", |b| {
+        b.iter(|| t!("a.very.nested.message", name = "Jason", msg = "Bla bla"))
+    });
+}
+
+criterion_group!(benches, bench_t, bench_t_with_args);
+criterion_main!(benches);
