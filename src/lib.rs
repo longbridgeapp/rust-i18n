56cd37c409dc55669867a67f1ca5535fdbd321dc
Varphone Wong <varphone@qq.com>
2024-01-18 14:33:22 +0800

    Add new function `replace_patterns()` to speed up string replacement

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,72 @@ pub fn locale() -> Arc<String> {
     CURRENT_LOCALE.clone_string()
 }
 
+/// Replace patterns and return a new string.
+///
+/// # Arguments
+///
+/// * `input` - The input string, containing patterns like `%{name}`.
+/// * `patterns` - The patterns to replace.
+/// * `values` - The values to replace.
+///
+/// # Example
+///
+/// ```
+/// # use rust_i18n::replace_patterns;
+/// let input = "Hello, %{name}!";
+/// let patterns = &["name"];
+/// let values = &["world".to_string()];
+/// let output = replace_patterns(input, patterns, values);
+/// assert_eq!(output, "Hello, world!");
+/// ```
+pub fn replace_patterns(input: &str, patterns: &[&str], values: &[String]) -> String {
+    let input_bytes = input.as_bytes();
+    let mut pattern_pos = smallvec::SmallVec::<[usize; 64]>::new();
+    let mut stage = 0;
+    for (i, &b) in input_bytes.iter().enumerate() {
+        match (stage, b) {
+            (1, b'{') => {
+                stage = 2;
+                pattern_pos.push(i);
+            }
+            (2, b'}') => {
+                stage = 0;
+                pattern_pos.push(i);
+            }
+            (_, b'%') => {
+                stage = 1;
+            }
+            _ => {}
+        }
+    }
+    let mut output: Vec<u8> = Vec::with_capacity(input_bytes.len() + 128);
+    let mut prev_end = 0;
+    let pattern_values = patterns.iter().zip(values.iter());
+    for pos in pattern_pos.chunks_exact(2) {
+        let start = pos[0];
+        let end = pos[1];
+        let key = &input_bytes[start + 1..end];
+        if prev_end < start {
+            let prev_chunk = &input_bytes[prev_end..start - 1];
+            output.extend_from_slice(prev_chunk);
+        }
+        if let Some((_, v)) = pattern_values
+            .clone()
+            .find(|(&pattern, _)| pattern.as_bytes() == key)
+        {
+            output.extend_from_slice(v.as_bytes());
+        } else {
+            output.extend_from_slice(&input_bytes[start - 1..end + 1]);
+        }
+        prev_end = end + 1;
+    }
+    if prev_end < input_bytes.len() {
+        let remaining = &input_bytes[prev_end..];
+        output.extend_from_slice(remaining);
+    }
+    unsafe { String::from_utf8_unchecked(output) }
+}
+
 /// Get I18n text
 ///
 /// ```no_run
