22e0609591a2c08930f52a0e6bc860f02a0e88c0
René Kijewski <Kijewski@users.noreply.github.com>
2024-01-22 12:09:36 +0100

    Use `arc_swap` to implement `AtomicStr` (#72)
    * Use `arc_swap` to implement `AtomicStr`
    
    The previous implementation allowed use-after-free in a multi-threaded
    context. This PR fixes the problem by using `ArcSwap`, which is
    implements thread-safe swapping of `Arc`s, and is widely used.
    
    * Use `triomphe::Arc` to mitigate performance losses
    
    This change replaces `std::sync::Arc` with `triomphe::Arc`. The latter
    has no weak references, and is a lot faster because of that.
    
    * Update atomic_str.rs
    
    * Update lib.rs
    
    * Update lib.rs
    
    ---------
    
    Co-authored-by: Jason Lee <huacnlee@gmail.com>

--- crates/support/src/atomic_str.rs
+++ crates/support/src/atomic_str.rs
@@ -1,80 +1,65 @@
 use std::fmt;
-use std::sync::atomic::{AtomicPtr, Ordering};
-use std::sync::Arc;
+use std::ops::Deref;
+
+use arc_swap::{ArcSwapAny, Guard};
+use triomphe::Arc;
 
 /// A thread-safe atomically reference-counting string.
-pub struct AtomicStr(AtomicPtr<String>);
+pub struct AtomicStr(ArcSwapAny<Arc<String>>);
+
+/// A thread-safe view the string that was stored when `AtomicStr::as_str()` was called.
+struct GuardedStr(Guard<Arc<String>>);
+
+impl Deref for GuardedStr {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.0.as_str()
+    }
+}
 
 impl AtomicStr {
     /// Create a new `AtomicStr` with the given value.
-    pub fn new(value: impl Into<String>) -> Self {
+    pub fn new(value: &str) -> Self {
         let arced = Arc::new(value.into());
-        Self(AtomicPtr::new(Arc::into_raw(arced) as _))
+        Self(ArcSwapAny::new(arced))
     }
 
     /// Get the string slice.
-    pub fn as_str(&self) -> &str {
-        unsafe {
-            let arced_ptr = self.0.load(Ordering::SeqCst);
-            assert!(!arced_ptr.is_null());
-            &*arced_ptr
-        }
+    pub fn as_str(&self) -> impl Deref<Target = str> {
+        GuardedStr(self.0.load())
     }
 
-    /// Get the cloned inner `Arc<String>`.
-    pub fn clone_string(&self) -> Arc<String> {
-        unsafe {
-            let arced_ptr = self.0.load(Ordering::SeqCst);
-            assert!(!arced_ptr.is_null());
-            Arc::increment_strong_count(arced_ptr);
-            Arc::from_raw(arced_ptr)
-        }
-    }
-
-    /// Replaces the value at self with src, returning the old value, without dropping either.
-    pub fn replace(&self, src: impl Into<String>) -> Arc<String> {
-        unsafe {
-            let arced_new = Arc::new(src.into());
-            let arced_old_ptr = self.0.swap(Arc::into_raw(arced_new) as _, Ordering::SeqCst);
-            assert!(!arced_old_ptr.is_null());
-            Arc::from_raw(arced_old_ptr)
-        }
+    /// Replaces the value at self with src.
+    pub fn replace(&self, src: impl Into<String>) {
+        let arced = Arc::new(src.into());
+        self.0.store(arced);
     }
 }
 
-impl Drop for AtomicStr {
-    fn drop(&mut self) {
-        unsafe {
-            let arced_ptr = self.0.swap(std::ptr::null_mut(), Ordering::SeqCst);
-            assert!(!arced_ptr.is_null());
-            let _ = Arc::from_raw(arced_ptr);
-        }
-    }
-}
-
-impl AsRef<str> for AtomicStr {
-    fn as_ref(&self) -> &str {
-        self.as_str()
-    }
-}
-
-impl<T> From<T> for AtomicStr
-where
-    T: Into<String>,
-{
-    fn from(value: T) -> Self {
+impl From<&str> for AtomicStr {
+    fn from(value: &str) -> Self {
         Self::new(value)
-    }
-}
-
-impl From<&AtomicStr> for Arc<String> {
-    fn from(value: &AtomicStr) -> Self {
-        value.clone_string()
     }
 }
 
 impl fmt::Display for AtomicStr {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.as_str())
+        f.write_str(&self.as_str())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_str(s: &str) {
+        assert_eq!(s, "hello");
+    }
+
+    #[test]
+    fn test_atomic_str() {
+        let s = AtomicStr::from("hello");
+        test_str(&s.as_str());
     }
 }
